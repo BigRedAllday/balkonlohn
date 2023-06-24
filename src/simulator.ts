55e3bb8278f930564786dc0462a08c352a9f61a2
s.kuehner <s.kuehner@enerparc.com>
2023-06-24 21:55:21 +0200

    getting metadata from feed in data

---
+++ src/simulator.ts
@@ -0,0 +1,40 @@
+import {ConsumptionProfile} from "./consumptionProfile";
+import {FeedInData} from "./feedInData";
+
+export class Simulator {
+    public run() {
+        // Feed in is limited for example by inverter power
+        const feedInLimit = 600;
+        const baseLoadIncrease = 0;
+
+        // Load Profiles
+        const profile = new ConsumptionProfile();
+        profile.loadProfiles("my_profile");
+        const feedInData = new FeedInData();
+        const feedInMetaData = feedInData.loadFeedInData("600-Watt_35-Degrees.csv");
+        console.log(`Simulating with ${JSON.stringify(feedInMetaData)}`);
+
+        // Variables needed by year-iteration
+        const start = new Date('2023-01-01T00:00:00.000Z');
+        const end = new Date('2023-12-31T23:00:00.000Z');
+        const quarterHour = 1000 * 60 * 15; // in Millisekunden
+
+        // Counter
+        let totalConsumption = 0;
+        let totalProduction = 0;
+        let selfConsumption = 0;
+
+        for (let current = start.getTime(); current <= end.getTime(); current += quarterHour) {
+            const currentTime = new Date(current);
+            const consumption = profile.getConsumption(currentTime) + baseLoadIncrease / 4;
+            const feedIn = Math.min(feedInData.getFeedIn(currentTime), feedInLimit);
+            totalConsumption = totalConsumption + consumption;
+            totalProduction = totalProduction + feedIn;
+            selfConsumption = selfConsumption + Math.min(consumption, feedIn);
+        }
+
+        console.log(`Jahresverbrauch laut Profil (kWh): ${totalConsumption / 1000}`);
+        console.log(`Produktion gesamt (kWh): ${totalProduction / 1000}`);
+        console.log(`Eigenverbrauch (kWh): ${selfConsumption / 1000}`);
+    }
+}
