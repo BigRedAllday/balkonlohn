9ef2bc7552716d5d8eaa9bc6b13fdd47d7901de0
s.kuehner <s.kuehner@enerparc.com>
2023-08-26 15:25:53 +0200

    after-merge

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import {SetPointType} from "./types";
 import * as process from "process";
 
 async function main() {
-    const simulator = new Simulator(600, 100);
+    const simulator = new Simulator(600, 0);
     const storageSize = process.argv[2] ? Number.parseInt(process.argv[2]) : 0;
     console.log(`Storage size: ${storageSize} Wh`);
     simulator.addStorage(storageSize, SetPointType.CONSUMPTION_PROFILE);
