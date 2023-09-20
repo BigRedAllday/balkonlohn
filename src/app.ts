9d094a1b7bc8d8a116ac6d0f7a71c5411155af2b
BigRedAllday <103949044+BigRedAllday@users.noreply.github.com>
2023-09-20 22:39:30 +0200

    Summertime (#6)

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,11 @@ import {SetPointType} from "./types";
 import * as process from "process";
 
 async function main() {
-    const simulator = new Simulator(600, 0);
+    const simulator = new Simulator(600, 150);
     const storageSize = process.argv[2] ? Number.parseInt(process.argv[2]) : 0;
     console.log(`Storage size: ${storageSize} Wh`);
     simulator.addStorage(storageSize, SetPointType.CONSUMPTION_PROFILE);
-    await simulator.run("shadow/22_full_covered.csv", "my_profile", true);
+    await simulator.run("shadow_summer/1-one_degree.csv", "my_profile", true);
 }
 
 //Invoke the main function
