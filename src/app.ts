9735948e6f011419bb71a0050610e12dd262a98d
s.kuehner <s.kuehner@enerparc.com>
2023-10-31 21:19:38 +0100

    paul and mia

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import {SetPointType} from "./types";
 import * as process from "process";
 
 async function main() {
-    const simulator = new Simulator(800, 0, 1);
+    const simulator = new Simulator(800, 5, 1);
     const storageSize = process.argv[2] ? Number.parseInt(process.argv[2]) : 0;
     console.log(`Storage size: ${storageSize} Wh`);
     simulator.addStorage(storageSize, SetPointType.CONSUMPTION_PROFILE);
