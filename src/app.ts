105388fc87e4780505c7901629b91e422c66f759
s.kuehner <s.kuehner@enerparc.com>
2023-11-01 22:15:39 +0100

    update paul mia

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,11 @@ import {SetPointType} from "./types";
 import * as process from "process";
 
 async function main() {
-    const simulator = new Simulator(800, 5, 1);
+    const simulator = new Simulator(800, 0, 1);
     const storageSize = process.argv[2] ? Number.parseInt(process.argv[2]) : 0;
     console.log(`Storage size: ${storageSize} Wh`);
     simulator.addStorage(storageSize, SetPointType.CONSUMPTION_PROFILE);
-    await simulator.run("1000-Watt_35-Degrees_Hamburg.csv", "paul_mia", false);
+    await simulator.run("1000-Watt_35-Degrees_Munich.csv", "paul_mia", false);
 }
 
 //Invoke the main function
