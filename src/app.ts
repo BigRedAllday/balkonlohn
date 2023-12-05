1c44a9fee8bc6db49f35fcaa6a39d58ac362970b
s.kuehner <s.kuehner@enerparc.com>
2023-12-05 20:39:59 +0100

    new calculations

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,11 @@ import {SetPointType} from "./types";
 import * as process from "process";
 
 async function main() {
-    const simulator = new Simulator(800, 0, 1);
+    const simulator = new Simulator(800, 130, 1);
     const storageSize = process.argv[2] ? Number.parseInt(process.argv[2]) : 0;
     console.log(`Storage size: ${storageSize} Wh`);
     simulator.addStorage(storageSize, SetPointType.CONSUMPTION_PROFILE);
-    await simulator.run("miaAndPaul/1000-Watt_35-Degrees_Emden.csv", "paul_mia", false);
+    await simulator.run("my_profile_optimal_angles/Timeseries_53.648_9.707_SA2_1kWp_crystSi_10_10deg_-90deg_2020_2020.csv", "my_profile", false);
 }
 
 //Invoke the main function
