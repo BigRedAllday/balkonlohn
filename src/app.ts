34ba5a176860b0793feb75197d0bc76347e6e4b3
s.kuehner <s.kuehner@enerparc.com>
2023-12-09 17:50:22 +0100

    more profiles

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,8 @@ async function main() {
     const storageSize = process.argv[2] ? Number.parseInt(process.argv[2]) : 0;
     console.log(`Storage size: ${storageSize} Wh`);
     simulator.addStorage(storageSize, SetPointType.CONSUMPTION_PROFILE);
-    await simulator.run("my_profile_optimal_angles/Timeseries_53.648_9.707_SA2_1kWp_crystSi_10_10deg_-90deg_2020_2020.csv", "my_profile", false);
+    await simulator.run("after_burner/Timeseries_53.649_9.704_SA2_1kWp_crystSi_10_90deg_-90deg_2020_2020.csv",
+        "my_profile", false);
 }
 
 //Invoke the main function
