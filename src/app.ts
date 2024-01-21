87b2caa9ddc29667910a12412a84e4a000de648f
s.kuehner <s.kuehner@enerparc.com>
2024-01-21 20:55:48 +0100

    solar gift feed in

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,11 @@ import {SetPointType} from "./types";
 import * as process from "process";
 
 async function main() {
-    const simulator = new Simulator(800, 130, 1);
+    const simulator = new Simulator(800, 800, 1);
     const storageSize = process.argv[2] ? Number.parseInt(process.argv[2]) : 0;
     console.log(`Storage size: ${storageSize} Wh`);
     simulator.addStorage(storageSize, SetPointType.CONSUMPTION_PROFILE, 25);
-    await simulator.run("after_burner/1000-Watt_40-Degrees.csv",
+    await simulator.run("solarpaket1/Kassel_4Kw.csv",
         "my_profile", false);
 }
 
